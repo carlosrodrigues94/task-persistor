0f2e60ceda6fc20045a52a28ce3f22e78f9e2865
carlosrodrigues94 <carlos.1994hrs@gmail.com>
2021-11-19 22:35:33 -0300

    fix: card height

--- src/components/card/styles.ts
+++ src/components/card/styles.ts
@@ -10,7 +10,7 @@ export const Container = styled.div<ContainerProps>`
   align-items: center;
   justify-content: flex-start;
   flex-direction: column;
-  height: 500px;
+  min-height: 500px;
   width: 350px;
   border-radius: 8px;
   border: 1px solid rgba(0, 0, 0, 0.15);
