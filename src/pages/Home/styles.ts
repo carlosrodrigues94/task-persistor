cc6d75b1b5d847166c635f3c3a25c64cbc5b9258
carlosrodrigues94 <carlos.1994hrs@gmail.com>
2021-12-15 09:30:33 -0300

    feat: cards alignment

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   display: flex;
-  align-items: center;
+  align-items: flex-start;
   justify-content: center;
   min-height: 100vh;
   color: #fff;
