31f73ab9d0bb45fb9e020cf1916cecd87176fc6a
carlosrodrigues94 <carlos.1994hrs@gmail.com>
2022-07-01 19:55:43 -0300

    change: vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ const path = require("path");
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
-    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
   plugins: [react()],
   build: {
